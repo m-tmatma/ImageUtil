2118495cb76a096c7295e8c47d5a98c0f8b6a757
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-01-04 12:46:11 +0900

    bmp ファイルの分割を実装

--- ImageUtil/Program.cs
+++ ImageUtil/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace ImageUtil
 {
@@ -10,6 +12,42 @@ namespace ImageUtil
     {
         static void Main(string[] args)
         {
+            if (args.Length < 2)
+            {
+                return;
+            }
+            var input = args[0];
+            var outdir = args[1];
+            var testout = input + ".png";
+
+            if (!Directory.Exists(outdir))
+            {
+                Directory.CreateDirectory(outdir);
+            }
+
+            var bmp = (Bitmap)Image.FromFile(input);
+            bmp.Save(testout, System.Drawing.Imaging.ImageFormat.Png);
+
+            var width = bmp.Size.Width;
+            var height = bmp.Size.Height;
+            var sx = 16;
+            var sy = 16;
+
+            var index = 1;
+            for (var y = 0; y < height; y += sy)
+            {
+                for (var x = 0; x < width; x += sx)
+                {
+                    Console.WriteLine("{0}, {1}, {2}, {3}, {4}", index, x, y, x + sx, y + sy);
+                    var cloneRect = new RectangleF(x, y, sx, sy);
+                    using (var cloneBitmap = bmp.Clone(cloneRect, System.Drawing.Imaging.PixelFormat.Format4bppIndexed))
+                    {
+                        var outfile = Path.Combine(outdir, index.ToString() + ".bmp");
+                        cloneBitmap.Save(outfile, System.Drawing.Imaging.ImageFormat.Bmp);
+                    }
+                    index++;
+                }
+            }
         }
     }
 }
